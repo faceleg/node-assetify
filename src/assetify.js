33695196ade2a70232d77e286737db366becb085
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-15 13:57:44 -0300

    updated defaults

--- src/assetify.js
+++ src/assetify.js
@@ -6,7 +6,8 @@ var extend = require('xtend'),
     defaults = {
         development: process.env.NODE_ENV === 'development',
         appendTo: global,
-        js: []
+        js: [],
+        css: []
     },
     config; // module configuration options
 
