198e8a49671fafe943877c0c4b5a93117756ceb5
bevacqua <nicolasbevacqua@gmail.com>
2013-01-14 23:56:37 -0300

    config vars

--- src/assetify.js
+++ src/assetify.js
@@ -4,21 +4,35 @@ var extend = require('xtend'),
         development: process.env.NODE_ENV === 'development',
         appendTo: 'global',
         js: [],
-        base: '/static',
+        in: '/static',
         out: '/assets'
-    };
+    },
+    config; // module configuration options
 
-function output(opts, paths){
+function configure(opts){
+    if(config !== undefined){
+        throw new Error('assetify can only be configured once.');
+    }
+    if(opts.base === undefined){
+        throw new Error('opts.base is required. e.g: __dirname');
+    }
+
+    config = extend(defaults, opts);
+    config.input = config.base + config.in;
+    config.output = config.base + config.out;
+}
+
+function output(paths){
     paths.forEach(function(path){
-        disk.copy(opts.base + path, opts.out + path);
+        disk.copy(config.input + path, config.output + path);
     });
 }
 
 var api = {
     publish: function(opts){
-        var config = extend(defaults, opts);
+        configure(opts);
 
-        output(config, config.js);
+        output(config.js);
     }
 };
 
