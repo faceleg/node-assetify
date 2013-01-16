ad559c49b1b3d72a4274f5226f66a39ec12f6f32
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-16 18:28:13 -0300

    ver 0.0.4 with coffee support

---
+++ src/plugins/coffee.js
@@ -0,0 +1,11 @@
+var path = require('path'),
+    coffee = require('coffee-script'),
+    parser = require('./parser.js');
+
+module.exports = parser.configure('js',['.coffee'], function(item, config, ctx, done){
+    var filename = path.join(config.source, item.local),
+        includes = path.dirname(filename);
+
+    item.src = coffee.compile(item.src.toString());
+    done();
+});
