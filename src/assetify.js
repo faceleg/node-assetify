14ee41885cf72f1a12451e878f716432eaa7866c
Nico Bevacqua <nicolasbevacqua@gmail.com>
2013-01-16 11:35:40 -0300

    improved version of assetify

--- src/assetify.js
+++ src/assetify.js
@@ -29,147 +29,144 @@ function configure(opts){
         throw new Error("opts.source can't be the same as opts.bin");
     }
 
-    config.concat = config.production === true; // no reason this should be overwritable.
-    config.minify = config.production === true; // no reason this should be overwritable.
+    config.bundle = config.production === true; // no reason this should be overwritable.
 }
 
 function readFilesAsync(items, cb){
-    async.forEach(items, function(item, callback){
-        var complex = typeof item === 'object',
-            normalized = complex ? item : {
-                local: item
+    async.forEach(items, function(source, callback){
+        var complex = typeof source === 'object',
+            item = complex ? source : {
+                local: source
             };
 
-        var i = items.indexOf(item);
-        items[i] = normalized;
+        var i = items.indexOf(source);
+        items[i] = item;
 
-        if(normalized.local !== undefined){ // local might not exist.
-            var source = path.join(config.source, normalized.local);
+        if(item.local !== undefined){ // local might not exist.
+            var file = path.join(config.source, item.local);
 
-            fs.readFile(source, function(err, data){
+            fs.readFile(file, function(err, data){
                 if(err){
                     throw err;
                 }
                 item.src = data;
+                item.path = path.join(config.bin, item.local);
                 callback(err);
             });
         }
     }, cb);
 }
 
-function bundleAsync(items, cb){
-    var profiles = profileNamesDistinct(sources),
+function bundleAsync(items, key, cb){
+    if(config.bundle === false){
+        return cb(null); // leave as-is.
+    }
+    var profiles = profileNamesDistinct(items),
         bundles = [];
 
-    async.forEach(profiles, function(profile, callback){
-        var bundle = {
-            profile: profile,
-            items: [],
-            sources: [],
-            external: []
-        };
+    return async.forEach(profiles, function(profile, done){
+        var filename = profile + '.' + key,
+            bundle = {
+                profile: profile,
+                local: filename,
+                path: path.join(config.bin, filename),
+                sources: []
+            };
 
         items.forEach(function(item){
-           if(item.profile === undefined || item.profile === profile){
-               if(item.ext === undefined){
-                   if(item.src === undefined){
-                       throw new Error('item has no source nor is an external resource');
-                   }
-                   bundle.sources.push(item);
-               }else{
-                   bundle.external.push(item);
-               }
-           }
+            if(item.profile === undefined || item.profile === profile){
+                if(item.ext === undefined){
+                    if(item.src === undefined){
+                        throw new Error('item has no source nor is an external resource');
+                    }
+                    bundle.sources.push(item.src);
+                }else{
+                    if (bundles.indexOf(item) === -1){
+                        bundles.push(item); // keep external resources unaltered.
+                    }
+                }
+            }
         });
 
+        bundle.src = bundle.sources.join('\n');
         bundles.push(bundle);
-        callback();
+        return done();
     }, function(err){
         if(err){
             throw err;
         }
-        cb(null, bundles);
-    });
-}
-
-function endCopyTasks(err, sources, targets, extension, cb){
-    if(err){
-        throw err;
-    }
-    if(config.concat === true){
-        var profiles = profileNamesDistinct(sources);
-        async.forEach(profiles, function(profile, callback){
-            var concat = [];
-            sources.forEach(function(target){
-                if(target.profile === undefined || target.profile === profile){
-                    if(target.ext === undefined){
-                        concat.push(target.src);
-                    }else{
-                        if (targets.indexOf(target) === -1){ // sanity for sources without explicit profile.
-                            targets.push(target);
-
-                            var source = path.join(config.source, target.local);
-                            var local = path.join(config.bin, target.local);
-                            disk.copySafe(source, local);
-                        }
-                    }
-                }
-            });
-            var profileName = (profile || 'all') + '.' + extension,
-                bundle = path.join(config.bin, profileName),
-                code = concat.join('\n');
-
-            targets.push({
-                profile: profile,
-                local: profileName
-            });
-            disk.write(bundle, code, callback);
-        }, function(err){
-            if(err){
-                throw err;
-            }
-            cb(targets);
+        items.splice(0, items.length);
+        bundles.forEach(function(bundle){
+            items.push(bundle);
         });
-    }else{
-        cb(sources);
-    }
-}
-
-function concatenateAsync(bundles){
-    var physical = [];
-    async.forEach(bundles, function(bundle, callback){
-        if(bundle.external.length){
-            physical.push(bundle); // pass-through
-        }else{
-
-        }
+        cb(null);
     });
 }
 
-function normalizeAndCopyOver(items, cb, extension){
+function outputAsync(items, cb){
+    async.forEach(items, function(item, callback){
+        disk.write(item.path, item.src, callback);
+    },function(err){
+        if(err){
+            throw err;
+        }
+        cb(null);
+    });
+}
+
+function raise(key, eventName, items, done){
+    // TODO: plugins to minify and process input files
+    done(null);
+}
+
+function readBundleAndOutput(items, key, cb){
     fse.remove(config.bin, function(){
         readFilesAsync(items, function(err){
             if(err){
                 throw err;
             }
-            bundleAsync(items, function(err, bundles){
+
+            raise(key, 'afterReadFile', items, bundle);
+
+            function bundle(err){
                 if(err){
                     throw err;
                 }
-                concatenateAsync(bundles);
-            });
-            async.parallel(tasks, function(err){
-                endCopyTasks(err, sources, targets, extension, cb);
-            });
+                bundleAsync(items, key, function(err){
+                    if(err){
+                        throw err;
+                    }
+                    raise(key, 'afterBundle', items, copy);
+                });
+            }
+
+            function copy(err){
+                if(err){
+                    throw err;
+                }
+                outputAsync(items, function(err){
+                    if(err){
+                        throw err;
+                    }
+                    raise(key, 'afterOutput', items, done);
+                });
+            }
+
+            function done(err){
+                if(err){
+                    throw err;
+                }
+                cb(items);
+            }
         });
     });
 }
 
-function profileNamesDistinct(sources){
+function profileNamesDistinct(items){
     var names = [undefined];
-    sources.forEach(function(target){
-        if (names.indexOf(target.profile) === -1){
-            names.push(target.profile);
+    items.forEach(function(item){
+        if (names.indexOf(item.profile) === -1){
+            names.push(item.profile);
         }
     });
     if(names.length > 1){ // if using profiles, don't save a common profile.
@@ -190,38 +187,38 @@ function profile(tags){
     };
 }
 
-function renderTags(targets, opts){
+function renderTags(items, opts){
     var tags = [];
-    targets.forEach(function(target){
-        var external = target.ext !== undefined,
-            href = external ? target.ext : target.local;
+    items.forEach(function(item){
+        var external = item.ext !== undefined,
+            href = external ? item.ext : item.local;
 
         if(!external && href.indexOf('/') !== 0){
             href = '/' + href;
         }
 
         var tag = opts.render(href);
-        tags.push({ html: tag, profile: target.profile });
+        tags.push({ html: tag, profile: item.profile });
 
-        (opts.then || function(){})(target, tags);
+        (opts.then || function(){})(item, tags);
     });
 
     return profile(tags);
 }
 
-function scriptTags(targets){
-    function then(target, tags){
-        if(target.ext !== undefined && target.local !== undefined){
-            if(target.test === undefined){
+function scriptTags(items){
+    function then(item, tags){
+        if(item.ext !== undefined && item.local !== undefined){
+            if(item.test === undefined){
                 throw new Error('fallback test is missing');
             }
-            var code = target.test + ' || document.write(unescape("%3Cscript src=\'' + target.local + '\'%3E%3C/script%3E"))';
+            var code = item.test + ' || document.write(unescape("%3Cscript src=\'' + item.local + '\'%3E%3C/script%3E"))';
             var fallback = '<script>' + code +  '</script>';
-            tags.push({ html: fallback, profile: target.profile });
+            tags.push({ html: fallback, profile: item.profile });
         }
     }
 
-    return renderTags(targets, {
+    return renderTags(items, {
         render: function(href){
             return '<script src="' + href + '"></script>'
         },
@@ -229,24 +226,18 @@ function scriptTags(targets){
     });
 }
 
-function styleTags(targets){
-    return renderTags(targets, {
+function styleTags(items){
+    return renderTags(items, {
         render: function(href){
             return '<link rel="stylesheet" href="' + href + '">';
         }
     })
 }
 
-function expose(key, value){
-    config.appendTo[key] = value;
-}
-
-function process(sources, tag, key){
-    normalizeAndCopyOver(sources, function(results){
-        var tags = tag(results);
-
-        expose(key, tags);
-    }, key);
+function process(items, tag, key){
+    readBundleAndOutput(items, key, function(results){
+        config.appendTo[key] = tag(results);
+    });
 }
 
 var api = {
