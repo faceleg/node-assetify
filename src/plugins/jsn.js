d55c3a5f99c3f3f7d6874d43c24d1278a2d92fa1
bevacqua <nicolasbevacqua@gmail.com>
2013-01-20 13:42:20 -0300

    jsn support

--- src/plugins/jsn.js
+++ src/plugins/jsn.js
@@ -1,12 +1,13 @@
 var path = require('path'),
+    jsn = require('jsn'),
     parser = require('./parser.js');
 
 module.exports = parser.configure('js',['.jsn'], function(item, config, ctx, done){
-    parser.parse(item.src.toString(), function (err, tree) {
+    jsn.parse(item.src.toString(), item.context, function (err, js) {
         if(err){
             throw err;
         }
-        item.src = tree.toCSS();
+        item.src = js;
         done();
     });
 });
