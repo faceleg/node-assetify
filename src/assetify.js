b53bbd395978c0f13d402d1d7dc4bc0effaf5447
bevacqua <nicolasbevacqua@gmail.com>
2013-01-24 00:03:09 -0300

    debug mode for jq

--- src/assetify.js
+++ src/assetify.js
@@ -3,9 +3,9 @@ var api = {
     middleware: require('./middleware.js').initialize,
     use: require('./plugins/framework.js').register,
     plugins: require('./plugins/all.js'),
-    jQuery: function(version, local, profile){
+    jQuery: function(version, local, profile, debug){
         return {
-            ext: '//ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery.min.js',
+            ext: '//ajax.googleapis.com/ajax/libs/jquery/' + version + '/jquery' + (debug ? '' : '.min') + '.js',
             local: local,
             test: 'window.jQuery',
             profile: profile
