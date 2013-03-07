5d89ba0b63ab15927b4b0f3ba61fe8945837868e
bevacqua <nicolasbevacqua@gmail.com>
2013-03-07 00:34:39 -0300

    implemented fingerprinting, closing #1

--- src/plugins/fingerprint.js
+++ src/plugins/fingerprint.js
@@ -4,14 +4,14 @@ var async = require('async'),
 module.exports = {
     events: [{
         eventName: 'beforeRender',
-        plugin: function(items, config, ctx, callback){
+        plugin: function(items, config, ctx, done){
             if(!ctx.http.req.assetFingerprint){
                 throw new Error('You forgot to add the static-asset middleware to your application, it is necessary for this plugin to work.');
             }
             (items || []).forEach(function(item){
                 item.out = ctx.http.req.assetFingerprint(item.out);
             });
-            callback();
+            done();
         }
     }]
 };
